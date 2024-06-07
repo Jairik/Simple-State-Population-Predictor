f8276442009ad34919bfc04d6221623c525efcea
Jairik <jairikmc@gmail.com>
2024-06-07 11:05:49 -0400

    Added df manipulation
    Added df manipulation so it can be properly passed into the model and visualization portion. Also updated readme

--- PopulationPredictor.py
+++ PopulationPredictor.py
@@ -7,14 +7,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, r2_score
 
+#Retrive the data from the website
+print("loading population data...")
 url = 'https://www.census.gov/data/tables/time-series/dec/popchange-data-text.html'
 ssl._create_default_https_context = ssl._create_unverified_context
 scraper = pd.read_html(url)
+df = scraper[0]
 
-#Determine table layout (it is just one big one)
-# for i, table in enumerate(scraper):
-#     print("----------------------------------------------------")
-#     print(i)
-#     print(table)
 
-print(scraper)
+    
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Basic State Population Predictor
- Used as an elementary learning exercise, this project will pull state population statistics from the web and predict future populations.
+ Elementary learning exercise for python web scraping, visualization, and ML. This program will
+
+ - Pull state population data off of https://www.census.gov/data/tables/time-series/dec/popchange-data-text.html and clean it
+ - Prompt the user to select a state to predict
+ - Run a linear regression model to predict the future state population
+ - Visualize past populations and the new predicted population using a bar chart
